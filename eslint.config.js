50ca7fcff5397a54d839cd917cfd2070b359f638
Pavel Melnik <melnikpavel298@gmail.com>
2026-02-01 01:43:34 +0300

    [new] config project

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import eslintConfigPrettier from '@eslint-config-prettier'
-import js from '@eslint/js'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,4 +22,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
