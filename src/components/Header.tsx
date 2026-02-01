14b9d1d875d6daf83dbde255f0a3ec739df91064
Pavel Melnik <melnikpavel298@gmail.com>
2026-02-01 15:51:24 +0300

    [fix] fix anchor link scroll with new route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleNavigation = (hash: string) => (e: React.MouseEvent) => {
+    if (location.pathname !== '/') {
+      e.preventDefault();
+      navigate(`/#${hash}`);
+    }
+  };
+
   return (
     <header className="h-[80px] w-full bg-blue-500">
       <div className="container h-full">
         <div className="h-full flex justify-between items-center">
-          <h1 className="">Intergrio</h1>
+          <h1 className="inter text-2xl font-bold">Intergrio</h1>
 
           <nav aria-label="Основная навигация">
             <ul className="flex items-center gap-6">
               <li>
-                <a href="#hero">Home</a>
+                <a onClick={handleNavigation('hero')} href="#hero">
+                  Home
+                </a>
               </li>
               <li>
-                <a href="#demo">Demo</a>
+                <a onClick={handleNavigation('demo')} href="#demo">
+                  Demo
+                </a>
               </li>
               <li>
-                <a href="#pricing">Pricing</a>
+                <a onClick={handleNavigation('pricing')} href="#pricing">
+                  Pricing
+                </a>
               </li>
               <li>
-                <a href="#about">About</a>
+                <a onClick={handleNavigation('about')} href="#about">
+                  About
+                </a>
               </li>
               <li>
                 <Link to="/login">Login</Link>
